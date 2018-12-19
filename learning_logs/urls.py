d8ce97d07c50ea43f933bc21732f8cedc3625ea8
ythvg <446339431@qq.com>
2018-12-19 09:13:35 +0800

    add Entries

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     re_path(r'^topics/$', views.topics, name='topics'),
     re_path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
     re_path(r'^new_topic/$', views.new_topic, name='new_topic'),
+    re_path(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from .forms import Topic, TopicForm
+from .models import Topic
+from .forms import TopicForm, EntryForm
 
 def index(request):
     """学习笔记的主页"""
@@ -33,3 +34,21 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+def new_entry(request, topic_id):
+    """在特定的主题中添加信条目"""
+    topic = Topic.objects.get(id=topic_id)
+    print('fdasfdasf')
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic',
+                                                args=[topic_id]))
+    
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
