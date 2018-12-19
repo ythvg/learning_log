0739edb07363f3629cfcac1f9734a3185b1d2547
ythvg <446339431@qq.com>
2018-12-19 11:52:25 +0800

    add user register

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     re_path(r'^login/$', auth_views.LoginView.as_view(template_name='users/login.html'),
             name='login'),
     re_path(r'^logout/$', views.logout_view, name='logout'),
+    re_path(r'^register/$', views.register, name='register'),
 ]
+
